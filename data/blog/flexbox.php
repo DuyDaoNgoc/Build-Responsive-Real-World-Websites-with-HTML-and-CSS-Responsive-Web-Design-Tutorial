3b8ebebf9f4f1fe26b4c90372c10a0cd5b48f034
DuyDaoNgoc <kinbingo@gmail.com>
2024-11-19 23:29:29 +0700

    Build Responsive Real-World Websites with HTML and CSS | Responsive Web Design Tutorial

---
+++ data/blog/flexbox.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="flexbox.css">
+    <title>Document</title>
+</head>
+<body>
+    <h1>FLEXBOX</h1>
+    <div class="container">
+        <div class="div1">HTML</div>
+        <div class="div2">AND</div>
+        <div class="div3">CSS</div>
+        <div class="div4">ARE</div>
+        <div class="div5">AMAZING</div>
+        <div class="div6">LANGUAGE</div>
+        <div class="div7">TO</div>
+        <div class="div8">LEARN</div>
+    </div>
+<h1>GRID</h1>
+    <div class="container2">
+        <div class="div1">HTML</div>
+        <div class="div2">AND</div>
+        <div class="div3">CSS</div>
+        <div class="div4">ARE</div>
+        <div class="div5">AMAZING</div>
+        <div class="div6">LANGUAGE</div>
+        <div class="div7">TO</div>
+        <div class="div8">LEARN</div>
+    </div>
+</body>
+</html>
---
+++ data/data_sql_tk/data_SQL.php
@@ -0,0 +1,46 @@
+
+
+<?php   
+// Thông tin kết nối cơ sở dữ liệu
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "thong_tin_tk";  
+
+// Tạo kết nối
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Kiểm tra kết nối
+if ($conn->connect_error) {
+die("Kết nối thất bại: " . $conn->connect_error);
+}
+
+// Kiểm tra nếu dữ liệu được gửi qua phương thức POST
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+// Lấy dữ liệu từ biểu mẫu
+$name = $taikhoan;
+$namsinh = $_POST['namsinh'];
+$songuoi_theodoi = $_POST['nguoitheodoi'];
+$bai_viet_da_dang = $_POST['dadang'];
+$lich_su_da_xem = $_POST['lichsu'];
+$nguoi_xem_hs = $_POST['nguoi_xemhs'];
+
+// Tạo câu Lệnh SQL để thêm dữ liệu
+$sql = "INSERT INTO trangcanhan_tk (tk,namsinh,nguoitheodoi,dadang,lichsu,nguoi_xemhs) 
+VALUES ('$name', '$namsinh', '$songuo i_theodoi',$bai_viet_da_dang, $lich_su_da_xem ,$nguoi_xem_hs)";
+
+// Thực thi câu lệnh SQL và kiểm tra kết quả
+
+if ($conn->query($sql) === TRUE) {
+    echo "Thêm dữ liệu thành công!";
+} else {
+    echo "Lỗi: " . $sql . "<br>" . $conn->error;
+}
+}
+
+$conn->close();
+?>
+    </html> 
+
+
+
